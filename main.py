e2589a9a5a65b187e4ca0c2c4852317e697d8185
romoch82 <romoch37@gmail.com>
2024-12-01 19:04:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(int((9**0.5)*5))
+
+#2nd program
+print(9.99 > 9.98 and 1000 < 1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4th program
+a = '123.456'
+b = float(a)
+print(int(b *10)% 10)
